39caab224ed6932ab79658f86310ed60919af158
alecorlo <alecorlo1234@gmail.com>
2026-01-05 16:49:22 -0600

    Agregar archivos de proyecto.

---
+++ GastoClass/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GastoClass
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GastoClass/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GastoClass
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
