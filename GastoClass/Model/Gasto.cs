758143a20c07d45cffa36c069a48a0ec1b282d4f
alecorlo <alecorlo1234@gmail.com>
2026-01-05 18:31:28 -0600

    Creacion de Clase Monto

---
+++ GastoClass/Model/Gasto.cs
@@ -0,0 +1,19 @@
+﻿namespace GastoClass.Model
+{
+    /// <summary>
+    /// Clase contendra el gasto ejemplo
+    /// 1
+    /// 2000
+    /// 05/01/2026
+    /// Coca Cola
+    /// Ocio
+    /// </summary>
+    public class Gasto
+    {
+        public int Id { get; set; }
+        public decimal Mondo { get; set; }
+        public DateTime Fecha { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Categoria { get; set; }
+    }
+}
