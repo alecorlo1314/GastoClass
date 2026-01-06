caa0bee92a6434b10ff67ae82b84b6fcc67dc4b3
alecorlo <alecorlo1234@gmail.com>
2026-01-05 22:33:32 -0600

    Implementación de repositorios y DI para gestión de gastos
    - Se corrigió el nombre de la propiedad 'Mondo' a 'Monto' en la clase Gasto.
    - Se implementó la clase DatosGastos con métodos para obtener gastos totales del mes y se añadieron métodos placeholder para futuras funcionalidades.
    - Se creó RepositorioBaseDatos para gestionar la conexión SQLite y la creación de la tabla Gasto.
    - Se configuró la inyección de dependencias en MauiProgram.cs para vistas, viewmodels, repositorios y servicios.
    - Se realizaron limpiezas y ajustes de namespaces en varios archivos para mejorar la organización del código.

---
+++ GastoClass/Infraestructura/Repositorios/DatosGastos.cs
@@ -0,0 +1,58 @@
+﻿using GastoClass.Dominio.Interfacez;
+using GastoClass.Model;
+
+namespace GastoClass.Infraestructura.Repositorios
+{
+    public class DatosGastos : IServicioGastos
+    {
+        //Inyeccion de dependecias
+        private readonly RepositorioBaseDatos _repositorioBaseDatos;
+        public DatosGastos(RepositorioBaseDatos repositorioBaseDatos)
+        {
+            _repositorioBaseDatos = repositorioBaseDatos;
+        }
+
+        public async Task<decimal> ObtenerGastosTotalesDelMesAsync(int mes, int anio)
+        {
+            try
+            {
+                //obtener la conexion a la base de datos
+                var conexion = await _repositorioBaseDatos.ObtenerConexion();
+
+                //var total = await conexion.ExecuteScalarAsync<float>("SELECT SUM(Monto) FROM Gasto");
+
+                //Consulta para obtener los gastos del mes y año especificados
+                var consulta = conexion.Table<Gasto>()
+                    .Where(g => g.Fecha.Month == mes && g.Fecha.Year == anio);
+                //lo convertimos en lista asyncrona
+                var gastosDelMes = await consulta.ToListAsync();
+                //inicializamos el total de gastos
+                decimal totalGastos = 0;
+                //iteramos sobre los gastos y sumamos los montos
+                foreach (var gasto in gastosDelMes)
+                {
+                    totalGastos += gasto.Monto;
+                }
+                //retornamos el total de gastos
+                return totalGastos;
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                throw new Exception("Error al obtener los gastos totales del mes.", ex);
+            }
+        }
+        public Task<List<int>> ObtenerTransaccionesDelMesAsync(int mes, int anio)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<List<string>> ObtenerCategoriasMayorGastoDelMess(int mes, int anio)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<List<GastoClass.Model.Gasto>> ObtenerUltimos5GastosAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GastoClass/MauiProgram.cs
+++ GastoClass/MauiProgram.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
+using GastoClass.Infraestructura.Repositorios;
+using GastoClass.View;
+using GastoClass.Presentacion.View;
+using GastoClass.ViewModel;
+using GastoClass.Aplicacion.CasosUso;
+using GastoClass.Dominio.Interfacez;
 
 namespace GastoClass
 {
@@ -14,9 +20,27 @@ namespace GastoClass
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            //vistas
+            builder.Services.AddTransient<AgregarGastoPage>();
+            builder.Services.AddTransient<ConfiguracionesPage>();
+            builder.Services.AddTransient<DashboardPage>();
+            builder.Services.AddTransient<HistorialGastosPage>();
+            builder.Services.AddTransient<MLDetallesPage>();
+            //ViewModels
+            builder.Services.AddTransient<AgregarGastoViewModel>();
+            builder.Services.AddTransient<ConfiguracionesViewModel>();
+            builder.Services.AddTransient<DashboardViewModel>();
+            builder.Services.AddTransient<HistorialGastosViewModel>();
+            builder.Services.AddTransient<MLDetallesViewModel>();
+
+            //Repositorios y servicios
+            builder.Services.AddSingleton<DatosGastos>();
+            builder.Services.AddSingleton<RepositorioBaseDatos>();
+            builder.Services.AddSingleton<IServicioGastos, DatosGastos>();
+            builder.Services.AddSingleton<ServicioGastos>();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
