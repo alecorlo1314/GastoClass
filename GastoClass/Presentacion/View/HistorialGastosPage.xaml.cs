df7e9fd9f2d8b88eec878a1bfd1f52363932a9ab
alecorlo <alecorlo1234@gmail.com>
2026-01-12 17:43:17 -0600

    Se realizo la consulta y vizualicion de los mivimientos en la pantalla de historial

--- GastoClass/Presentacion/View/HistorialGastosPage.xaml.cs
+++ GastoClass/Presentacion/View/HistorialGastosPage.xaml.cs
@@ -1,9 +1,14 @@
+using GastoClass.Presentacion.ViewModel;
+
 namespace GastoClass.Presentacion.View;
 
 public partial class HistorialGastosPage : ContentPage
 {
-	public HistorialGastosPage()
+	public HistorialGastosPage(HistorialGastosViewModel historialGastosViewModel)
 	{
 		InitializeComponent();
-	}
+        //Inyeccion de Dependencias
+        BindingContext = historialGastosViewModel;
+
+    }
 }
--- GastoClass/Presentacion/ViewModel/HistorialGastosViewModel.cs
+++ GastoClass/Presentacion/ViewModel/HistorialGastosViewModel.cs
@@ -1,7 +1,45 @@
-﻿
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using GastoClass.Aplicacion.CasosUso;
+using GastoClass.Dominio.Model;
+using System.Collections.ObjectModel;
+
 namespace GastoClass.Presentacion.ViewModel
 {
-    internal class HistorialGastosViewModel
+    public partial class HistorialGastosViewModel : ObservableObject
     {
+        //Inyeccion de Dependencias
+        private readonly ServicioGastos _gastoService;
+
+        #region Listas Observables
+        [ObservableProperty]
+        private ObservableCollection<Gasto>? listaGastos = new();
+        #endregion
+        public HistorialGastosViewModel(ServicioGastos gastoService)
+        {
+            //Inyeccion de Dependencias
+            _gastoService = gastoService;
+            //Cargar lista de gastos
+            //Task.Run(async () => await CargarListaMovimientos());
+            _ = CargarListaMovimientos();
+        }
+        //Cargar lista de gastos desde la base de datos
+        private async Task CargarListaMovimientos()
+        {
+            try
+            {
+                var consulta = await _gastoService.ObtenerGastosAsync();
+                //Limpiamos la lista
+                ListaGastos?.Clear();
+                //cargar la lista de gastos con la consulta
+                foreach (var gasto in consulta)
+                {
+                    ListaGastos?.Add(gasto);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
