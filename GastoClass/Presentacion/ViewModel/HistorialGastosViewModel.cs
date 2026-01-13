3d0b620eff95218a1ef59e6ee2a74588105ab256
alecorlo <alecorlo1234@gmail.com>
2026-01-12 19:34:17 -0600

    Se realizo la logica de busqueda por descripcion, id, categoria y monto

--- GastoClass/Presentacion/ViewModel/HistorialGastosViewModel.cs
+++ GastoClass/Presentacion/ViewModel/HistorialGastosViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using GastoClass.Aplicacion.CasosUso;
 using GastoClass.Dominio.Model;
 using System.Collections.ObjectModel;
@@ -13,7 +14,12 @@ namespace GastoClass.Presentacion.ViewModel
         #region Listas Observables
         [ObservableProperty]
         private ObservableCollection<Gasto>? listaGastos = new();
+        [ObservableProperty]
+        private ObservableCollection<Gasto>? listaGastosFiltrados = new();
         #endregion
+        //Variables 
+        [ObservableProperty]
+        private string? textoBusqueda;
         public HistorialGastosViewModel(ServicioGastos gastoService)
         {
             //Inyeccion de Dependencias
@@ -35,10 +41,44 @@ namespace GastoClass.Presentacion.ViewModel
                 {
                     ListaGastos?.Add(gasto);
                 }
+                ListaGastosFiltrados = new ObservableCollection<Gasto>(ListaGastos!);
             }
             catch (Exception ex)
             {
 
+            }
+        }
+
+        partial void OnTextoBusquedaChanged(string? value)
+        {
+            _ = Filtrar(value);
+        }
+        private async Task Filtrar(string? textoBusqueda)
+       {
+            try
+            {
+                //Paso
+                // - verificar el texto no este vacio
+                // - Si esta vacio, mostrar toda la lista
+                // - Si tiene texto, filtrar la lista por descripcion, categoria o monto
+                // - Actualizar la lista observable
+                if (string.IsNullOrWhiteSpace(textoBusqueda))
+                {
+                    ListaGastosFiltrados = new ObservableCollection<Gasto>(ListaGastos!);
+                    return;
+                }
+                textoBusqueda = textoBusqueda.ToLower();
+                var resultado = ListaGastos?.Where(g =>
+            g.Descripcion!.ToLower().Contains(textoBusqueda) ||
+            g.Id!.ToString().Contains(textoBusqueda) ||
+            g.Categoria!.ToLower().Contains(textoBusqueda) ||
+            g.Monto.ToString().Contains(textoBusqueda));
+
+                ListaGastosFiltrados = new ObservableCollection<Gasto>(resultado!);
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlertAsync("Error", ex.Message, "OK");
             }
         }
     }
