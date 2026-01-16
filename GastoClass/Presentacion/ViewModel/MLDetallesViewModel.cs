72a8932272c7f4dd447ef775c634694410cd62ea
alecorlo <alecorlo1234@gmail.com>
2026-01-15 23:41:29 -0600

    Inyeccion de dependencias de vmMLDetalles a servicios de la api de prediccion ML

--- GastoClass/Presentacion/ViewModel/MLDetallesViewModel.cs
+++ GastoClass/Presentacion/ViewModel/MLDetallesViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace GastoClass.Presentacion.ViewModel
+﻿using GastoClass.Aplicacion.CasosUso;
+
+namespace GastoClass.Presentacion.ViewModel
 {
     public class MLDetallesViewModel
     {
+        #region Inyeccion de dependencias
+        /// <summary>
+        /// Para obtener los servicios las predicciones
+        /// </summary>
+        private readonly PredictionApiService _predictionApiService;
+        #endregion
+        public MLDetallesViewModel(PredictionApiService predictionApiService)
+        {
+            //Inyeccion de dependencias
+            _predictionApiService = predictionApiService;
+        }
     }
 }
