8d08148af2ec518e7fba6a30efa8be96d6615bb1
alecorlo <alecorlo1234@gmail.com>
2026-01-19 11:30:05 -0600

    Se agregaron nuevas propiedades observables para almacenar datos relacionados con una tarjeta de crédito, como NombreTarjeta, NumeroTarjeta, FechaVencimiento, LimiteCredito, TipoMoneda, DiaCorte, DiaPago, NombreBanco, ColorHexa1 y ColorHexa2. Se agregó una nueva lista observable ListaTipoTarjeta y ListaTipoMoneda para almacenar tipos de tarjetas y monedas. Se agregaron nuevos métodos y comandos, como ColorTarjeta, FinalizarRegistroTarjetaCredito, OnTipoTarjetaChanged, OnNombreTarjetaChanged, OnUltimosCuatroDigitosChanged, OnTipoMonedaChanged, OnDiaCorteChanged y OnDiaPagoChanged. Se agregaron nuevas propiedades para almacenar mensajes de validación, como MensajeRequerimientoTipoTarjeta, MensajeRequerimientoNombreTarjeta, MensajeRequerimientoUltimosCuatroDigitos, MensajeRequerimientoFechaVencimiento, MensajeRequerimientoLimiteCredito, MensajeRequerimientoTipoMoneda, MensajeRequerimientoDiaPago y MensajeRequerimientoDiaCorte. Se agregó un nuevo constructor que inicializa las listas de tipos de tarjetas y monedas.

--- GastoClass/Presentacion/ViewModel/TarjetaCreditoViewModel.cs
+++ GastoClass/Presentacion/ViewModel/TarjetaCreditoViewModel.cs
@@ -43,30 +43,117 @@ namespace GastoClass.Presentacion.ViewModel
         /// </summary>
         [ObservableProperty]
         private DateTime? fechaVencimiento;
+
+        /// <summary>
+        /// Contiene los datos del limite de credito
+        /// </summary>
+        [ObservableProperty]
+        private decimal? limiteCredito;
+
+        /// <summary>
+        /// Contiene el tipo de moneda USA(Dolar) o CRC(Colones)
+        /// </summary>
+        [ObservableProperty]
+        private TipoMoneda? tipoMoneda;
+
+        /// <summary>
+        /// Contiene el dia de corte
+        /// </summary>
+        [ObservableProperty]
+        private int? diaCorte;
+
+        /// <summary>
+        /// Contiene el dia de pago
+        /// </summary>
+        [ObservableProperty]
+        private int? diaPago;
+
+        [ObservableProperty]
+        private string? nombreBanco;
         #endregion
 
-        #region ListasObservables
+        #region Listas Observables
+        /// <summary>
+        /// Contiene la lista de tipos de tarjeta
+        /// </summary>
         [ObservableProperty]
         private ObservableCollection<TipoTarjeta>? listaTipoTarjeta = new();
+
+        /// <summary>
+        /// Contiene la lista de tipos de moneda
+        /// </summary>
+        [ObservableProperty]
+        private ObservableCollection<TipoMoneda>? listaTipoMoneda = new();
         #endregion
 
         #region Propiedades Logicas
+        /// <summary>
+        /// Indica si el bono de ver mas detalles es accesible
+        /// </summary>
         [ObservableProperty]
         private bool? masDetallesEsAccesible = false;
+
+        /// <summary>
+        /// Indica si el bono de finalizar el registro es accesible
+        /// </summary>
+        [ObservableProperty]
+        private bool? finalizarRegistroAccesible = false;
         #endregion
 
         #region Mensajes
+        /// <summary>
+        /// Contiene el mensaje de requerimiento de ultimos cuatro digitos
+        /// </summary>
         [ObservableProperty]
         private string? mensajeRequerimientoUltimosCuatroDigitos;
 
+        /// <summary>
+        /// Contiene el mensaje de requerimiento de tipo de tarjeta
+        /// </summary>
         [ObservableProperty]
         private string? mensajeRequerimientoTipoTarjeta;
 
+        /// <summary>
+        /// Contiene el mensaje de requerimiento de nombre de tarjeta
+        /// </summary>
         [ObservableProperty]
         private string? mensajeRequerimientoNombreTarjeta;
 
+        /// <summary>
+        /// Contiene el mensaje de requerimiento de fecha de vencimiento
+        /// </summary>
         [ObservableProperty]
         private string? mensajeRequerimientoFechaVencimiento;
+
+        /// <summary>
+        /// Contiene el mensaje de requerimiento de limite de credito
+        /// </summary>
+        [ObservableProperty]
+        private string? mensajeRequerimientoLimiteCredito;
+
+        /// <summary>
+        /// Contiene el mensaje de requerimiento de tipo de moneda
+        /// </summary>
+        [ObservableProperty]
+        private string? mensajeRequerimientoTipoMoneda;
+
+        /// <summary>
+        /// Contiene el mensaje de requerimiento de dia de pago
+        /// </summary>
+        [ObservableProperty]
+        private string? mensajeRequerimientoDiaPago;
+
+        /// <summary>
+        /// Contiene el mensaje de requerimiento de dia de corte
+        /// </summary>
+        [ObservableProperty]
+        private string? mensajeRequerimientoDiaCorte;
+
+        /// <summary>
+        /// Contiene el mensaje de requerimiento de nombre del banco
+        /// </summary>
+        [ObservableProperty]
+        private string? mensajeRequerimientoNombreBanco;
         #endregion
 
         #region Constructor
@@ -79,9 +166,21 @@ namespace GastoClass.Presentacion.ViewModel
                 new(){ Tipo = "Visa"},
                 new() { Tipo = "Amex"}
             };
+            //Inicializamos la lista de tipos de moneda
+            ListaTipoMoneda = new ObservableCollection<TipoMoneda>
+            {
+                new(){ Moneda = "CRC"},
+                new(){ Moneda = "USD"}
+            };
         }
         #endregion
 
+        #region Commandos
+        /// <summary>
+        /// Se ejecuta al tocar los bordes de colores para guardar los colores en las propiedades Hexadecimales
+        /// </summary>
+        /// <param name="borderSeleccionado"></param>
+        /// <returns></returns>
         [RelayCommand]
         private async Task ColorTarjeta(Border borderSeleccionado)
         {
@@ -97,8 +196,18 @@ namespace GastoClass.Presentacion.ViewModel
                 await Shell.Current.CurrentPage.DisplayAlertAsync("Color", $"Color Border: {solid.Color.ToHex()}, ColorHex1={ColorHexa1}, ColorHex2={ColorHexa2}", "OK");
             }
         }
+        [RelayCommand]
+        private async Task FinalizarRegistroTarjetaCredito()
+        {
+            //Inicializamos un objeto TarjetaCredito
+        }
+        #endregion
 
         #region Metodo Observables
+        /// <summary>
+        /// Realiza la validacion para el sfComboBox 
+        /// </summary>
+        /// <param name="value"></param>
         partial void OnTipoTarjetaChanged(TipoTarjeta? value)
         {
             if (value == null)
@@ -109,8 +218,12 @@ namespace GastoClass.Presentacion.ViewModel
                 return;
             }
             MensajeRequerimientoTipoTarjeta = string.Empty;
-            masDetallesEsAccesible = true;
+            MasDetallesEsAccesible = true;
         }
+        /// <summary>
+        /// Realiza la validacion para el nombre de la tarjeta
+        /// </summary>
+        /// <param name="value"></param>
         partial void OnNombreTarjetaChanged(string? value)
         {
             if (string.IsNullOrWhiteSpace(value) || value.Length <= 2)
@@ -123,6 +236,10 @@ namespace GastoClass.Presentacion.ViewModel
             MensajeRequerimientoNombreTarjeta = string.Empty;
             MasDetallesEsAccesible = true;
         }
+        /// <summary>
+        /// Realiza la validacion para los ultimos cuatro digitos
+        /// </summary>
+        /// <param name="value"></param>
         partial void OnUltimosCuatroDigitosChanged(int? value)
         {
             //Validacion de longitud
@@ -136,6 +253,10 @@ namespace GastoClass.Presentacion.ViewModel
             MensajeRequerimientoUltimosCuatroDigitos = string.Empty;
             MasDetallesEsAccesible = true;
         }
+        /// <summary>
+        /// Realiza la validacion para la fecha de vencimiento
+        /// </summary>
+        /// <param name="value"></param>
         partial void OnFechaVencimientoChanged(DateTime? value)
         {
             if (value < DateTime.Now)
@@ -147,6 +268,105 @@ namespace GastoClass.Presentacion.ViewModel
             }
             MasDetallesEsAccesible = true;
             MensajeRequerimientoFechaVencimiento = string.Empty;
+        }
+        /// <summary>
+        /// Realiza la validacion para el limite de credito
+        /// </summary>
+        /// <param name="value"></param>
+        partial void OnLimiteCreditoChanged(decimal? value)
+        {
+            if (value == 0)
+            {
+                finalizarRegistroAccesible = false;
+                MensajeRequerimientoLimiteCredito = "No puede ser 0";
+                return;
+            }
+            if (value < 0)
+            {
+                finalizarRegistroAccesible = false;
+                MensajeRequerimientoLimiteCredito = "No puede ser negativo";
+                return;
+            }
+            finalizarRegistroAccesible = true;
+            MensajeRequerimientoLimiteCredito = string.Empty;
+        }
+        /// <summary>
+        /// Realiza la validacion para el tipo de moneda
+        /// </summary>
+        /// <param name="value"></param>
+        partial void OnTipoMonedaChanged(TipoMoneda? value)
+        {
+            if (value == null)
+            {
+                //bloquear el bono de ver mas detalles antes de sacar el popup de agregar tarjeta
+                MensajeRequerimientoTipoMoneda = "Debes seleccionar un tipo de moneda";
+                finalizarRegistroAccesible = false;
+                return;
+            }
+            MensajeRequerimientoTipoMoneda = string.Empty;
+            finalizarRegistroAccesible = true;
+        }
+        /// <summary>
+        /// Realiza la validacion para el dia de corte
+        /// </summary>
+        /// <param name="value"></param>
+        partial void OnDiaCorteChanged(int? value)
+        {
+            if(value == null)
+            {
+                //bloquear el bono de ver mas detalles antes de sacar el popup de agregar tarjeta
+                finalizarRegistroAccesible = false;
+                MensajeRequerimientoDiaCorte = "Debes seleccionar un dia";
+                return;
+            }
+            if(value < 1 || value > 31)
+            {
+                //bloquear el bono de ver mas detalles antes de sacar el popup de agregar tarjeta
+                finalizarRegistroAccesible = false;
+                MensajeRequerimientoDiaCorte = "El dia debe estar entre 1 y 31";
+                return;
+            }
+            finalizarRegistroAccesible = true;
+            MensajeRequerimientoDiaCorte = string.Empty;
+        }
+        /// <summary>
+        /// Realiza la validacion para el dia de pago
+        /// </summary>
+        /// <param name="value"></param>
+        partial void OnDiaPagoChanged(int? value)
+        {
+            if (value == null)
+            {
+                //bloquear el bono de ver mas detalles antes de sacar el popup de agregar tarjeta
+                finalizarRegistroAccesible = false;
+                MensajeRequerimientoDiaPago = "Debes seleccionar un dia";
+                return;
+            }
+            if (value < 1 || value > 31)
+            {
+                //bloquear el bono de ver mas detalles antes de sacar el popup de agregar tarjeta
+                finalizarRegistroAccesible = false;
+                MensajeRequerimientoDiaPago = "El dia debe estar entre 1 y 31";
+                return;
+            }
+            finalizarRegistroAccesible = true;
+            MensajeRequerimientoDiaPago = string.Empty;
+        }
+        /// <summary>
+        /// Realiza la validacion para el nombre del banco
+        /// </summary>
+        /// <param name="value"></param>
+        partial void OnNombreBancoChanged(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value) || value.Length <= 2)
+            {
+                //bloquear el bono de ver mas detalles antes de sacar el popup de agregar tarjeta
+                finalizarRegistroAccesible = false;
+                MensajeRequerimientoNombreBanco = "Tiene que tener mas de 2 caracteres";
+                return;
+            }
+            MensajeRequerimientoNombreBanco = string.Empty;
+            finalizarRegistroAccesible = true;
         }
     }
     #endregion
