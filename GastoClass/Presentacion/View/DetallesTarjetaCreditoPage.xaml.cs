41b4d073729cc65dbcf235e5abca5bf0af0a9ee2
alecorlo <alecorlo1234@gmail.com>
2026-01-20 22:50:49 -0600

    Se agrego la vista de tarjeta de credito detalles

---
+++ GastoClass/Presentacion/View/DetallesTarjetaCreditoPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace GastoClass.Presentacion.View;
+
+public partial class DetallesTarjetaCreditoPage : ContentPage
+{
+	public DetallesTarjetaCreditoPage()
+	{
+		InitializeComponent();
+	}
+}
