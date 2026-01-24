f76459a1ff846e649129fce896c3fc92fb6e80a6
alecorlo <alecorlo1234@gmail.com>
2026-01-23 23:44:55 -0600

    se implemento la logica para que el titulo de la tarjetas sea visible si hay tarjetas

--- GastoClass/Presentacion/ViewModel/TarjetaCreditoViewModel.cs
+++ GastoClass/Presentacion/ViewModel/TarjetaCreditoViewModel.cs
@@ -199,6 +199,9 @@ namespace GastoClass.Presentacion.ViewModel
 
         [ObservableProperty]
         private bool? borderVisible;
+
+        [ObservableProperty]
+        private bool? tituloTarjetasVisible;
         #endregion
 
         #region Mensajes
@@ -293,6 +296,7 @@ namespace GastoClass.Presentacion.ViewModel
                 var tarjetas = await _servicioTarjetaCredito.ObtenerTarjetasCreditoAsync();
                 ListaTarjetasCredito = new ObservableCollection<TarjetaCredito>(tarjetas!);
                 BorderVisible = tarjetas?.Count == 0 || tarjetas == null ? false : true;
+                TituloTarjetasVisible = tarjetas?.Count == 0 || tarjetas == null ? false : true;
             }
             catch (Exception ex)
             {
