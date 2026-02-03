56125a861676c2a570ab8c5ef65bd214d973a46a
alecorlo <alecorlo1234@gmail.com>
2026-02-03 17:43:17 -0600

    Se eliminaron validaciones redundantes y se garantizó que el tipo de tarjeta siempre sea válido.

--- GastoClass/GastoClass.Dominio/Excepciones/ExcepcionesTarjetaCredito/ExcepcionTipoTarjetaInvalido.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GastoClass.Dominio.Excepciones.ExcepcionesTarjetaCredito;
-
-public class ExcepcionTipoTarjetaInvalido : ExcepcionDominio
-{
-    public ExcepcionTipoTarjetaInvalido(string campo, string? mensaje) : base(campo, mensaje)
-    {
-    }
-}
