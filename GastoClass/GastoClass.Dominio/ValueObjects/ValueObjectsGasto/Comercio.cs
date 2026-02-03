81d18c4ab5f46ed265bfece4694eaea5c753325a
alecorlo <alecorlo1234@gmail.com>
2026-02-03 17:01:09 -0600

    se corrigio el  de comercio

--- GastoClass/GastoClass.Dominio/ValueObjects/ValueObjectsGasto/Comercio.cs
+++ GastoClass/GastoClass.Dominio/ValueObjects/ValueObjectsGasto/Comercio.cs
@@ -1,27 +1,21 @@
 ﻿using GastoClass.Dominio.Excepciones.ExcepcionesGasto;
 using GastoClass.GastoClass.Dominio.Excepciones.ExcepcionesGasto;
 
-namespace GastoClass.GastoClass.Dominio.ValueObjects.ValueObjectsGasto;
-
 public readonly record struct Comercio
 {
-    public string? Valor { get; }
+    public string Valor { get; }
 
     public Comercio(string valor)
     {
         if (string.IsNullOrWhiteSpace(valor))
-        {
-            throw new ExcepcionComercioRequerido(nameof(valor), "Comercio es requerido");
-        }
-        if (string.IsNullOrEmpty(valor))
-        {
-            throw new ExcepcionComercioRequerido(nameof(valor), "Comercio es requerido");
-        } 
+            throw new ExcepcionComercioRequerido(nameof(Valor), "El comercio es requerido");
+
         if (valor.Length > 100)
-        {
-            throw new ExcepcionComercioMaximoCaracteres(nameof(valor), "Comercio debe tener un maximo de 100 caracteres");
-        }
-        Valor = valor;
+            throw new ExcepcionComercioMaximoCaracteres(nameof(Valor),
+                "El comercio no puede tener más de 100 caracteres");
+
+        Valor = valor.Trim();
     }
 
+    public override string ToString() => Valor;
 }
