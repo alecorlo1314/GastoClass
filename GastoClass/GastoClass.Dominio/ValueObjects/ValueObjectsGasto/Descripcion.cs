ae41731b4ede69359a9d6952b4d26af1bccde71d
alecorlo <alecorlo1234@gmail.com>
2026-02-03 17:02:04 -0600

    Se corrigio el de descripcion

--- GastoClass/GastoClass.Dominio/ValueObjects/ValueObjectsGasto/Descripcion.cs
+++ GastoClass/GastoClass.Dominio/ValueObjects/ValueObjectsGasto/Descripcion.cs
@@ -1,27 +1,26 @@
 ﻿using GastoClass.Dominio.Excepciones.ExcepcionesGasto;
 
-namespace GastoClass.GastoClass.Dominio.ValueObjects.ValueObjectsGasto;
-
 public readonly record struct Descripcion
 {
     public string Valor { get; }
 
     public Descripcion(string valor)
     {
-        if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrEmpty(valor))
-        {
-            throw new ExcepcionDescripcionInvalida(nameof(valor), "Descripcion es requerida");
-        }
-        Valor = valor.Trim();
-        if (valor.Length > 200)
-        {
-            throw new ExcepcionDescripcionInvalida(nameof(valor), "No puede ser mayor a 200 caracteres");
-        }
+        if (string.IsNullOrWhiteSpace(valor))
+            throw new ExcepcionDescripcionInvalida(nameof(Valor), "La descripción es requerida");
+
+        valor = valor.Trim();
+
         if (valor.Length < 3)
-        {
-            throw new ExcepcionDescripcionInvalida(nameof(valor), "No puede ser menor a 3 caracteres");
-        }
+            throw new ExcepcionDescripcionInvalida(nameof(Valor),
+                "La descripción debe tener al menos 3 caracteres");
+
+        if (valor.Length > 200)
+            throw new ExcepcionDescripcionInvalida(nameof(Valor),
+                "La descripción no puede superar los 200 caracteres");
+
         Valor = valor;
     }
+
     public override string ToString() => Valor;
 }
