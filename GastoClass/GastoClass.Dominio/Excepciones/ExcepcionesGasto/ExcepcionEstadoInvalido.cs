55af358ecb7ffff6d95150a2e570d260b3feb551
alecorlo <alecorlo1234@gmail.com>
2026-02-03 17:17:45 -0600

    Se elimino la excepcion de estado invalido y se usa la excepcion del dominio

--- GastoClass/GastoClass.Dominio/Excepciones/ExcepcionesGasto/ExcepcionEstadoInvalido.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GastoClass.Dominio.Excepciones.ExcepcionesGasto;
-
-public class ExcepcionEstadoInvalido : ExcepcionDominio
-{
-    public ExcepcionEstadoInvalido(string campo, string? mensaje) : base(campo, mensaje)
-    {
-    }
-}
