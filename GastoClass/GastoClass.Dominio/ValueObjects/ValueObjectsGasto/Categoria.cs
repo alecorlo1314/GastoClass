39b47abd70cebd616df0ba981bfa037930483d3a
alecorlo <alecorlo1234@gmail.com>
2026-02-03 16:59:18 -0600

    Se corrigio el de Categoria

--- GastoClass/GastoClass.Dominio/ValueObjects/ValueObjectsGasto/Categoria.cs
+++ GastoClass/GastoClass.Dominio/ValueObjects/ValueObjectsGasto/Categoria.cs
@@ -1,18 +1,16 @@
 ﻿using GastoClass.Dominio.Excepciones.ExcepcionesGasto;
 
-namespace GastoClass.GastoClass.Dominio.ValueObjects.ValueObjectsGasto;
-
 public readonly record struct Categoria
 {
     public string Valor { get; }
 
-    public Categoria(string? valor)
+    public Categoria(string valor)
     {
         if (string.IsNullOrWhiteSpace(valor))
-        {
-            throw new ExcepcionCategoriaInvalida(nameof(valor), "Categoria es requerida");
-        }
-        Valor = valor;
+            throw new ExcepcionCategoriaInvalida(nameof(Valor), "La categoría es requerida");
+
+        Valor = valor.Trim();
     }
+
     public override string ToString() => Valor;
 }
