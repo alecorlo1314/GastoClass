c5331b301ea8ffc34accdf74c1037f3e6a402c31
alecorlo <alecorlo1234@gmail.com>
2026-02-03 17:06:10 -0600

    Se corrigio el de fecha

--- GastoClass/GastoClass.Dominio/ValueObjects/ValueObjectsGasto/Fecha.cs
+++ GastoClass/GastoClass.Dominio/ValueObjects/ValueObjectsGasto/Fecha.cs
@@ -1,24 +1,19 @@
 ﻿using GastoClass.Dominio.Excepciones.ExcepcionesGasto;
 
-namespace GastoClass.GastoClass.Dominio.ValueObjects.ValueObjectsGasto;
-
 public readonly record struct Fecha
 {
-    public DateTime? Valor { get;}
-    public Fecha(DateTime? fecha)
+    public DateTime Valor { get; }
+
+    public Fecha(DateTime fecha)
     {
-        if(fecha!.Value.Year < 2024)
-        {
-            throw new ExcepcionFechaInvalida(nameof(fecha),"No puede ser menor al 2024");
-        }
+        if (fecha.Year < 2024)
+            throw new ExcepcionFechaInvalida(nameof(Valor),
+                "La fecha no puede ser menor al año 2024");
+
         if (fecha > DateTime.Now)
-        {
-            throw new ExcepcionFechaInvalida(nameof(fecha), "No puede ser mayor a la fecha actual");
-        }
-        if (fecha == null)
-        {
-            throw new ExcepcionFechaInvalida(nameof(fecha), "No puede ser nulo");
-        }
+            throw new ExcepcionFechaInvalida(nameof(Valor),
+                "La fecha no puede ser mayor a la fecha actual");
+
         Valor = fecha;
     }
 }
