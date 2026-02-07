991b01e2ea67011550213dae99f493638f38f0ae
alecorlo <alecorlo1234@gmail.com>
2026-02-06 23:27:37 -0600

    Se implemento los filtros para gastos por categoria en los ultimos 7 dias de la semana

--- GastoClass.Aplicacion/Tarjeta/DTOs/GastoCategoriaUltimosSieteDiasTarjetaDto.cs
+++ GastoClass.Aplicacion/Tarjeta/DTOs/GastoCategoriaUltimosSieteDiasTarjetaDto.cs
@@ -2,7 +2,8 @@
 
 public class GastoCategoriaUltimosSieteDiasTarjetaDto
 {
-    public string? Dia { get; init; }
+    public string? DiaSemana { get; init; }
+    public int NumeroDia { get; init; }
     public string? Categoria { get; init; }
     public decimal TotalMonto { get; init; }
 }
