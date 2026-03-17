873736a163082132ca74c6cc1691ce212001ad79
alecorlo <alecorlo1234@gmail.com>
2026-03-17 16:16:26 -0600

    cambios reciente, agregador archivo .onnx en Recursos/raw para predicciones locales

---
+++ GastoClass.Aplicacion/Servicios/DTOs/TfidfData.cs
@@ -0,0 +1,9 @@
+﻿namespace GastoClass.Aplicacion.Servicios.DTOs;
+
+public class TfidfData
+{
+    public Dictionary<string, int> vocabulary { get; set; }
+    public List<double> idf { get; set; }
+    public int max_features { get; set; }
+    public List<int> ngram_range { get; set; }
+}
